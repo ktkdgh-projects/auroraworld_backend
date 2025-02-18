8161c021c335f34ec68daa2e45c9218fb5e8d987
ktkdgh <ktkdgh821@gmail.com>
2025-02-18 21:47:38 +0900

    feat: shares, weblinks db 추가

--- database/model/users.py
+++ database/model/users.py
@@ -5,11 +5,11 @@ class User(models.Model):
     username = models.CharField(max_length=255, unique=True) 
     password = models.BinaryField() 
     refresh = models.BinaryField(default=b'auroraworld')
-    createdAt = models.DateTimeField(auto_now_add=True) 
-    updatedAt = models.DateTimeField(auto_now=True)  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'user' 
+        db_table = 'users' 
 
     def __str__(self):
-        return f"User(id={self.id}, username={self.username}, password={self.password}, refresh={self.refresh}, createdAt={self.createdAt}, updatedAt={self.updatedAt})"
+        return f"User(id={self.id}, username={self.username}, password={self.password}, refresh={self.refresh}, created_at={self.created_at}, created_at={self.updated_at})"
